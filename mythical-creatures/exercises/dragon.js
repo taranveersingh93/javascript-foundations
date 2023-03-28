727df69340fe287e64aec5019b0a51f29c7a492f
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-27 20:47:17 -0400

    Solve dragon and vampire

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,47 @@
 
+function createDragon(dragonName, riderName, behavior) {
+  var dragon = {};
+  dragon.timesEaten = 0;
+  dragon.hungry = true;
 
+  if(arguments.length===1){
+    dragon.name = dragonName;
+  } else if(arguments.length===2){
+    dragon.name = dragonName;
+    dragon.rider = riderName;
+  } else {
+    dragon.name = dragonName;
+    dragon.rider = riderName;
+    dragon.temperment = behavior;
+  }
+  return dragon;
+}
+
+function greetRider(dragonObj) {
+  return `Hi, ${dragonObj.rider}!`;
+}
+
+function eat(dragonObj) {
+  dragonObj.timesEaten += 1;
+  if(dragonObj.timesEaten>=3) {
+    dragonObj.hungry = false;
+  } 
+  return dragonObj;
+}
+
+function findFireBreathers(allDragons) {
+  var fireBreathers = [];
+  for(var i=0; i<allDragons.length; i++) {
+    if(allDragons[i]["temperment"] === "aggressive") {
+      fireBreathers.push(allDragons[i]);
+    }
+  }
+  return fireBreathers;
+}
+ 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers
 }
--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,55 @@
+function createVampire(vampireName, petName) {
+  var vampire = {};
+  vampire.name = vampireName;
+  vampire.pet = "bat";
+  vampire.thirsty = true;
+  vampire.ouncesDrank = 0;
+  if(arguments.length===2) {
+    vampire.pet = petName;
+  }
+  return vampire;
+}
 
+function encounterDeliciousVictim(vampire) {
+  if(vampire.thirsty===true) {
+    return `I WANT TO SUCK YOUR BLOOD!`
+  } else {
+    return `No thanks, I am too full.`
+  }
+  
+}
 
+function drink(vampire) {
+  if(vampire.ouncesDrank<50 || vampire.thirsty === true) {
+    vampire.ouncesDrank += 10;
+  }
+  if(vampire.ouncesDrank<50) {
+    vampire.thirsty = false; 
+  }
+  return vampire;
+}
+
+function inquirePlace(locations, oneLocation) {
+  if(locations.includes(oneLocation)) {
+    return `Yes, I have spent some time in ${oneLocation}.`;
+  } else {
+    return `No, I have never been to ${oneLocation}.`;
+  }
+}
+
+function findBatLovers(vampires) {
+  var batLovers = [];
+  for(var i=0; i<vampires.length; i++) {
+    if(vampires[i].pet === "bat") {
+      batLovers.push(vampires[i].name);
+    }
+  }
+  return batLovers;
+}
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+  createVampire,
+  drink, 
+  findBatLovers, 
+  encounterDeliciousVictim,
+  inquirePlace
 }
