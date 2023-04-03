35f25ed8e93bff29fe5ea7c7b4cc8684dafc2d31
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-02 22:24:49 -0400

    Solve - barber exercises

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,44 @@
+function createBarber(inputName, inputEarnings, cutList) {
+    var barber = {
+        name: inputName,
+        earnings: 0,
+        haircuts: []
+    };
 
+    if(inputEarnings) {
+        barber.earnings = inputEarnings;
+    }
 
-module.exports = {  };
+    if(cutList) {
+        barber.haircuts = cutList
+    }
+
+    return barber; 
+}
+
+function giveCompliment(cut) {
+    return `This ${cut.style} looks great!`;
+}
+
+function cutHair(barber, cut) {
+    barber.haircuts.push(cut);
+    barber.earnings += cut.price;
+    return barber;
+}
+
+function listStyles(barber, length) {
+    var styles = [];
+    for (var i = 0; i < barber.haircuts.length; i++) {
+        if (barber.haircuts[i].hairLength === length) {
+            styles.push(barber.haircuts[i].style);
+        }
+    }
+    return styles;
+}
+
+module.exports = { 
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+ };
