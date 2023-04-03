4d0eabb2fff9d2440577f3b0632c606a95662834
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-02 20:15:02 -0400

    Solve calendar exercises

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,37 @@
+function createEvent(inputTask, inputMonth, inputDate) {
+    if (!inputDate || inputDate === 0 || inputDate > 31) {
+        return `Error: ${inputDate} is not a valid day`;
+    }
+    
+    var event = {};
+    event.title = inputTask;
+    event.month = inputMonth;
+    event.day = inputDate;
+    return event;
+}
 
+function createCalendar(inputOwner, inputEvents) {
+    var calendar = {
+        owner: inputOwner,
+        events: inputEvents
+    };
+    return calendar;
+}
 
-module.exports = {  };
+function reportMonthlyEvents(inputCalendar, inputMonth) {
+    
+    var monthlyEvents = [];
+    for(var i = 0; i < inputCalendar.events.length; i++) {
+        if(inputCalendar.events[i].month === inputMonth) {
+            console.log("condition met");
+            monthlyEvents.push(inputCalendar.events[i]);
+        }
+    }
+    return monthlyEvents;
+}
+
+module.exports = {
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+};
