089024c0658ba162ab43487500bb8afd370b02f5
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-03 17:08:20 -0400

    Solve - DJ exercises

--- dj/dj.js
+++ dj/dj.js
@@ -1,4 +1,41 @@
+function createSong(songName, songArtist) {
+    var song = {
+        name: songName,
+        artist: songArtist,
+        hasBeenPlayed: false
+    };
+    return song
+}
 
+function playSong(song) {
+    song.hasBeenPlayed = true;
+    return song
+}
 
+function makePlaylist(playlistName, songs) {
+    var playlist = {
+        name: playlistName,
+        songs: songs
+    }
+    return playlist;
+}
 
-module.exports = { };
+function addSongToPlaylist(playlist, song) {
+    playlist.songs.push(song);
+    return playlist;
+}
+
+function playAllSongs(playlist) {
+    for (var i=0; i < playlist.songs.length; i++) {
+        playlist.songs[i].hasBeenPlayed = true;
+    }
+    return playlist;
+}
+
+module.exports = {
+    createSong,
+    playSong,
+    makePlaylist,
+    addSongToPlaylist,
+    playAllSongs 
+ };
