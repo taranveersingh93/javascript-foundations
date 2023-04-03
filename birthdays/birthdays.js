a87d35b339a5ecba397830d6498754ca86092d27
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-03 19:59:04 -0400

    Solve - birthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,28 @@
+function createBirthday(inputName, inputMonth, inputDay) {
+    var birthday = {
+        name: inputName,
+        month: inputMonth,
+        day: inputDay
+    };
+    return birthday;
+}
 
+function celebrateBirthday(birthday) {
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month) {
+    var counter = 0;
+    for( var i = 0; i < birthdays.length; i++) {
+        if (birthdays[i].month === month) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+
+module.exports = { 
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+ };
