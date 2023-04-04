3f23a5e23377abe499418d6294d97b6565d6532b
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-03 21:31:05 -0400

    Solve - favorite foods

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,39 @@
+function createFavoriteFood(foodObject) {
+    var dish = {
+        name: foodObject.dish,
+        ingredients: foodObject.ingredients,
+        isSpicy: foodObject.isSpicy,
+        timesOrdered: 0
+    };
+    return dish;
+}
 
+function commentOnSpiciness(foodObject) {
+    if(foodObject.isSpicy === true) {
+        return `Wow, this ${foodObject.name} is so spicy!`
+    } else {
+        return `Phew, this ${foodObject.name} is not very spicy.`
+    }
+}
 
+function orderFood(dish) {
+    dish.timesOrdered += 1;
+    return dish;
+}
 
-module.exports = { };
+function createShoppingList(dishes) {
+    var allIngredients = [];
+    for(var i = 0; i < dishes.length; i++) {
+        for(var j = 0; j < dishes[i].ingredients.length; j++) {
+            allIngredients.push(dishes[i].ingredients[j]);
+        }
+    }
+    return allIngredients;
+}
+
+module.exports = {
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+ };
