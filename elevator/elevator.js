0d9ba956cdf00789216fa72ddcc7eb9906a1e845
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-03 21:53:51 -0400

    Solve - elevator exercises

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,33 @@
+function createElevator(inBuilding, inFloors, inCurrentFloor, inPassengers) {
+    var elevator = {
+        building: inBuilding,
+        floors: inFloors,
+        currentFloor: inCurrentFloor,
+        passengers: inPassengers
+    };
+    return elevator;
+}
+
+function changeFloors(inElevator, inDestinationFloor) {
+    if (inDestinationFloor > inElevator.floors) {
+        return `Floor ${inDestinationFloor} does not exist!`;
+    }
+    if (inElevator.currentFloor === inDestinationFloor) {
+        return `You're already on floor ${inDestinationFloor}!`;
+    } else {
+        return `Taking you to floor ${inDestinationFloor}!`;
+    }
+}
+
+function dropOffPassenger(inElevator, inPassenger) {
+    var index = inElevator.passengers.indexOf(inPassenger);
+    inElevator.passengers.splice(index,1);
+    return inElevator.passengers;
+}
 
 
-module.exports = {  };
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+ };
