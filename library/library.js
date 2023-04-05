6703adbc1d70ff285dc0833d1ffdf8cd405a4a18
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-04 22:38:15 -0400

    Solve - library

--- library/library.js
+++ library/library.js
@@ -1,3 +1,33 @@
+function issueCard(inName, inAge) {
+    var card = {
+        name: inName,
+        age: inAge,
+        numBooksCheckedOut: 0,
+        isChild: false
+    };
+    
+    if (card.age < 12) {
+        card.isChild = true;
+    }
 
+    return card;
+}
 
-module.exports = { };
+function searchByAuthor(inCollection, inAuthor) {
+    var resultArray = [];
+    for (var i = 0; i < inCollection.length; i++) {
+        if(inCollection[i].author === inAuthor) {
+            resultArray.push(inCollection[i]);
+        }
+    }
+    if(resultArray.length > 0) {
+        return resultArray;
+    } else {
+        return `No book found for search criteria`;
+    }
+}
+
+module.exports = { 
+    issueCard,
+    searchByAuthor
+ };
